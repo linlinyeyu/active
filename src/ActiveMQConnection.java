f9bd847a94c1bf1baf021722c9508e1411e69d33
ybliu <liuyubiaowu@qq.com>
2018-01-03 19:50:02 +0800

    de;te

--- src/ActiveMQConnection.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by linlinyeyu on 2017/12/29.
- */
-public class ActiveMQConnection {
-    public static final String DEFAULT_USER = "admin";
-    public static final String DEFAULT_PASSWORD = "admin";
-    public static final String DEFAULT_BROKER_URL = "http://localhost:8161";
-}
