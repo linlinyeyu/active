09c7e10088811de14b6dbbfa5e1e0e0691b5c086
ybliu <liuyubiaowu@qq.com>
2018-01-03 19:52:57 +0800

    udpate

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+import java.io.Serializable;
+
+/**
+ * Created by linlinyeyu on 2018/1/3.
+ */
+public class Person implements Serializable{
+    private static final Long serialVersionUID = 1L;
+
+    private String name;
+    private Integer age;
+    private String sex;
+
+    public Person(String name, Integer age, String sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
